e2633f1071cdd15722bfd57ee8c72c2cde492ae9
wayne-th <wayned.eth@gmail.com>
2021-10-23 15:13:17 -0400

    complete base64

--- src/app/components/tool/pdf/tool-pdf.component.ts
+++ src/app/components/tool/pdf/tool-pdf.component.ts
@@ -7,9 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ToolPdfComponent implements OnInit {
 
+  public isPdfUploaded: Boolean = false;
+  public pdfSrc: any;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  public uploadFile(event:any) {
+    let $img: any = document.querySelector('#upload-doc');
+    if(event.target.files[0].type == 'application/pdf'){
+      if (typeof (FileReader) !== 'undefined') {
+        let reader = new FileReader();
+        reader.onload = (e: any) => {
+          this.pdfSrc = e.target.result;
+        };
+        this.isPdfUploaded = true;
+        reader.readAsArrayBuffer($img.files[0]);
+      }
+    } else{
+      alert('Please upload pdf file')
+    }
   }
 
 }
